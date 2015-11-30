9601baa56d27ff5141d36e59994e84fa37dadf13
timwis <tim@timwis.com>
2015-11-29 21:22:18 -0500

    6 tests passing

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 var Parser = require('node-soda2-parser')
 
-module.exports = function (query) {
-  return {}
+module.exports = function (input) {
+  var ast = Parser.parse(input)
+  var query = {
+    outFields: '*',
+    outStatistics: [],
+    where: '1=1'
+  }
+
+  if (ast.columns instanceof Array) {
+    query.outFields = [] // recast as array
+    ast.columns.forEach(function (col) {
+      if (col.expr.type === 'column_ref') {
+        query.outFields.push(col.expr.column)
+      } else if (col.expr.type === 'aggr_func') {
+        query.outStatistics.push({
+          statisticType: col.expr.name,
+          onStatisticField: col.expr.args.expr.column,
+          outStatisticFieldName: col.as || col.expr.name + '_' + col.expr.args.expr.column
+        })
+      }
+    })
+  }
+
+  if (ast.groupby && ast.groupby.length) {
+    query.groupByFieldsForStatistics = ast.groupby[0].column
+  }
+
+  if (ast.where) {
+    query.where = Parser.stringify.where(ast.where)
+  }
+
+  return query
 }
--- test/soda-geoservices.js
+++ test/soda-geoservices.js
@@ -1,6 +1,9 @@
 /* global describe, it */
-var convert = require('../index')
 require('should')
+var convert = require('../index')
+/* var inspect = function (data) {
+  console.log(require('util').inspect(data, false, 10, true))
+} */
 
 describe('select', function () {
   it('multiple fields', function () {
@@ -8,55 +11,56 @@ describe('select', function () {
     query.should.have.property('outFields', ['foo', 'bar'])
   })
 
+  /* must be done in post-processing
   it('field aliases', function () {
     var query = convert('$select=foo AS bar')
-    // must be done in post-processing
-  })
+  })*/
 
-  it('operators', function () {
+  /* must be done in post-processing
+  (it('operators', function () {
     var query = convert('$select=foo * 3 AS bar')
-    // must be done in post-processing
-  })
+  })*/
 
   it('aggregation', function () {
     // count, sum, avg, stddev, var, min, max
-    var query = convert('$select=sum(foo)')
-    query.should.have.property('groupByFieldsForStatistics', 'foo')
+    var query = convert('$select=foo, sum(bar)')
     query.should.have.property('outStatistics', [{
       statisticType: 'sum',
-      onStatisticField: 'foo',
-      outStatisticFieldName: 'sum_foo'
+      onStatisticField: 'bar',
+      outStatisticFieldName: 'sum_bar'
     }])
   })
 
   it('aggregation with aliases', function () {
     var query = convert('$select=sum(foo) AS bar')
-    query.outStatistics.outStatisticFieldName.should.eql('bar')
+    query.outStatistics[0].should.have.property('outStatisticFieldName', 'bar')
   })
 
+  it('group by', function () {
+    var query = convert('$group=foo')
+    query.should.have.property('groupByFieldsForStatistics', 'foo')
+  })
+
+  /* not sure how to query geoservices this way
   it('date parts', function () {
     var query = convert('$select=date_trunc_ym(date)')
-    // not sure how to query geoservices this way
-  })
+  })*/
 
+  /* pretty sure this is supported via UPPER()/LOWER()
   it('lower/upper case', function () {
     var query = convert('$select=lower(foo)')
-    // pretty sure this is supported via UPPER()/LOWER()
-  })
+  })*/
 })
 
 describe('filter', function () {
   it('named filters', function () {
     var query = convert('foo=1&baz=quz')
-    query.should.have.property('where')
-    query.where.should.have.property('foo', 1)
-    query.where.should.have.property('baz', 'quz')
+    query.should.have.property('where', "foo = 1 AND baz = 'quz'")
   })
 
   it('$where filters', function () {
-    var query = convert('$where=foo = 1, baz=quz')
-    query.where.should.have.property('foo', 1)
-    query.where.should.have.property('baz', 'quz')
+    var query = convert('$where=foo = 1 and baz="quz"')
+    query.should.have.property('where', "foo = 1 AND baz = 'quz'")
   })
 })
 
